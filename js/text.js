7d7b2f2fa74683b6401b793ef72c0aa34c009db3
Vaibhav Mehrotra <31144001+vaibhavmehrotra94@users.noreply.github.com>
2018-07-08 22:46:50 +0530

    Initial commit

---
+++ js/text.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Comic Sans MS";
+ctx.fillStyle = "red";
+ctx.textAlign = "center";
+ctx.fillText("Try Pressing any Alphabet Key", canvas.width/2, canvas.height/2); 
